28b857a2bfcf718f8631ea5ab994908e6828692b
Your Name <ubaydullayev@samtuit.uz>
2022-03-23 10:58:26 +0500

    create_arr_ones_border_zeros.py

--- create_arr_ones_border_zeros.py
+++ create_arr_ones_border_zeros.py
@@ -1,3 +1,4 @@
+import numpy as np
 def create_arr_ones_border_zeros(n):
     """
     Create 2D array NxN of ones. Array border is zeros.
@@ -6,3 +7,8 @@ def create_arr_ones_border_zeros(n):
     Returns:
         list: 2D list
     """
+    arr = np.zeros((n,n))
+    arr[1:-1, 1:-1] = 1
+    return arr
+    
+print(create_arr_ones_border_zeros(5))
