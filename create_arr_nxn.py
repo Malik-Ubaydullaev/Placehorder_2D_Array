5858f189d1a935f70911ce1df451e10c5e09dc3f
Your Name <ubaydullayev@samtuit.uz>
2022-03-23 10:45:07 +0500

    create_arr_nxn.py

--- create_arr_nxn.py
+++ create_arr_nxn.py
@@ -7,3 +7,11 @@ def create_arr_nxn(n,k):
     Returns:
         list: 2D list
     """
+    arr = []
+    for i in range(n):
+        arr.append([])
+        for j in range(n):
+            arr[i].append(k)
+    return arr
+    
+print(create_arr_nxn(6, 3))
