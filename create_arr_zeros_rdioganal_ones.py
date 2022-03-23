28017328fc571084f3ec22491faa677fb8b2c858
Your Name <ubaydullayev@samtuit.uz>
2022-03-23 11:43:25 +0500

    create_arr_zeros_rdioganal_ones.py

--- create_arr_zeros_rdioganal_ones.py
+++ create_arr_zeros_rdioganal_ones.py
@@ -6,3 +6,15 @@ def create_arr_zeros_rdioganal_ones(n):
     Returns:
         list: 2D list
     """
+    arr = [] 
+    k = n
+    for i in range(n):
+        arr.append([])
+        for j in range(n):
+            if j == k - 1:
+                arr[i].append(1)
+            else:
+                arr[i].append(0)
+        k -= 1
+    return arr
+print(create_arr_zeros_rdioganal_ones(5))
