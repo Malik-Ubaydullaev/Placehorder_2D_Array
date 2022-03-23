cbe268071d26e73db959abb6a538981d4e8b4037
Your Name <ubaydullayev@samtuit.uz>
2022-03-23 11:29:22 +0500

    create_arr_ord_num.py

--- create_arr_ord_num.py
+++ create_arr_ord_num.py
@@ -6,4 +6,14 @@ def create_arr_ord_num(n,m):
         int: m
     Returns:
         list: 2D list
-  """
+    """
+    arr = [] 
+    k = 0
+    for i in range(n):
+        arr.append([])
+        for j in range(m):
+            arr[i].append(k+1)
+            k += 1
+    return arr
+print(create_arr_ord_num(4,3))
+
