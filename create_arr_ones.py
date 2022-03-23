28308a60e7d6a0da057f59655268d50ca404bb48
Your Name <ubaydullayev@samtuit.uz>
2022-03-23 11:07:28 +0500

    create_arr_ones.py

--- create_arr_ones.py
+++ create_arr_ones.py
@@ -1,3 +1,6 @@
+from numpy import array
+
+
 def create_arr_ones(n,m):
     """
     Create 2D array NxM of ones
@@ -7,3 +10,12 @@ def create_arr_ones(n,m):
     Returns:
         list: 2D list
     """
+    arr = []
+    for i in range(n):
+        arr.append([])
+        for j in range(n):
+            arr[i].append(1)
+    return arr
+    
+    return arr
+print(create_arr_ones(4,3))
